92cb0e5643a38229408595db8f53753deb151146
Kaleb <kaleb.stephens@fhnw.edu>
2026-04-10 09:21:23 -0600

    Added a welcome/homeview for the user

--- debtDash/debtDash/WelcomeView.swift
+++ debtDash/debtDash/WelcomeView.swift
@@ -9,7 +9,51 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+
+            Text("Welcome to\nDebtDash")
+                .font(.system(size: 36, weight: .bold))
+                .multilineTextAlignment(.center)
+                .foregroundColor(.black)
+
+            Text("Take control of your debt\nand track your progress easily.")
+                .font(.system(size: 18))
+                .multilineTextAlignment(.center)
+                .foregroundColor(.gray)
+                .padding(.top, 10)
+
+            Spacer()
+
+            NavigationLink {
+                DebtView()
+            } label: {
+                Text("Get Started")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 200, height: 55)
+                    .background(Color.black)
+                    .cornerRadius(16)
+            }
+
+            Spacer()
+
+            HStack {
+                Circle()
+                    .fill(Color.gray.opacity(0.35))
+                    .frame(width: 48, height: 48)
+
+                Spacer()
+
+                Text("Help")
+                    .foregroundColor(.black)
+                    .padding(.trailing, 10)
+            }
+            .padding(.horizontal, 20)
+            .padding(.bottom, 10)
+        }
+        .background(Color(.systemGray6))
+        .ignoresSafeArea()
     }
 }
 
