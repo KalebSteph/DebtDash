81af8ccd957fd90d7ec80503454e7a7564ff6e9b
Kaleb <kaleb.stephens@fhnw.edu>
2026-04-16 09:32:13 -0600

    Took out the welcome view and updated the debt goal page

---
+++ debtDash/debtDash/DebtGoal.swift
@@ -0,0 +1,18 @@
+//
+//  DebtGoal.swift
+//  debtDash
+//
+//  Created by Kaleb Stephens on 4/14/26.
+//
+
+import SwiftUI
+
+struct DebtGoal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DebtGoal()
+}
